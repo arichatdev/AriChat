a258fffe811a40e9cc0b40ed6960661d4e59ca71
arichatdev <ariadnersantos7@gmail.com>
2026-07-14 08:48:16 -0300

    script.js

--- script.js
+++ script.js
@@ -3,14 +3,53 @@ document.addEventListener("DOMContentLoaded", () => {
   const mensagens = document.getElementById("mensagens");
   const texto = document.getElementById("texto");
   const enviar = document.getElementById("enviar");
+  const digitando = document.getElementById("digitando");
+  const status = document.getElementById("status");
 
   const nome = "Ari";
+
+  // Mostrar status escolhido
+  status.addEventListener("change", () => {
+
+    const escolha = status.value;
+
+    if (escolha === "online") {
+      digitando.innerHTML = "🟢 Online";
+    }
+
+    if (escolha === "ocupado") {
+      digitando.innerHTML = "🔴 Ocupado";
+    }
+
+    if (escolha === "ausente") {
+      digitando.innerHTML = "🟡 Ausente";
+    }
+
+    if (escolha === "offline") {
+      digitando.innerHTML = "⚫ Offline";
+    }
+
+  });
+
+
+  // Aparece "digitando..."
+  texto.addEventListener("input", () => {
+
+    if (texto.value.length > 0) {
+      digitando.innerHTML = "✍️ Digitando...";
+    } else {
+      digitando.innerHTML = "🟢 Online";
+    }
+
+  });
+
 
   function enviarMensagem() {
 
     const mensagem = texto.value.trim();
 
     if (mensagem === "") return;
+
 
     const agora = new Date();
 
@@ -19,29 +58,41 @@ document.addEventListener("DOMContentLoaded", () => {
       minute: "2-digit"
     });
 
+
     const novaMensagem = document.createElement("div");
 
     novaMensagem.className = "mensagem";
+
 
     novaMensagem.innerHTML = `
       <strong>${nome}</strong><br>
       ${mensagem}
       <div class="hora">${hora}</div>
+      <div class="visualizado">✔✔ Visualizado</div>
     `;
+
 
     mensagens.appendChild(novaMensagem);
 
     texto.value = "";
 
+    digitando.innerHTML = "🟢 Online";
+
     mensagens.scrollTop = mensagens.scrollHeight;
+
   }
+
 
   enviar.addEventListener("click", enviarMensagem);
 
-  texto.addEventListener("keypress", function(e) {
-    if (e.key === "Enter") {
+
+  texto.addEventListener("keypress", (e)=>{
+
+    if(e.key === "Enter"){
       enviarMensagem();
     }
+
   });
+
 
 });
